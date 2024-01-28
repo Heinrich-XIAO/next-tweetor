c322414c5a631453aa773111d2395e888e397927
Heinrich-XIAO <heinrichxiao@gmail.com>
2024-01-28 18:41:23 -0500

    date shows in user time zone

--- components/flit.js
+++ components/flit.js
@@ -3,7 +3,7 @@ function Flit({ content, user, timestamp }) {
   // Convert to UCT so that js Date does not assume it is EST, + 1900 because getYear returns 1900 as 0
   timestamp = new Date(Date.UTC(timestamp.getYear() + 1900, timestamp.getMonth(), timestamp.getDay(), timestamp.getHours(), timestamp.getMinutes(), timestamp.getSeconds()));
   // Format to EST
-  timestamp = timestamp.toLocaleString('en-US', { timeZone: 'America/New_York', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'});
+  timestamp = timestamp.toLocaleString('en-US', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'});
   return (
     <div className="border border-l-0 p-5 flit">
 
